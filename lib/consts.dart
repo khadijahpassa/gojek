1a63ad70a8bd6b83337c342939e5a568a5e77b6d
khadijahpassa <khadijahpassa@gmail.com>
2024-11-25 14:00:04 +0700

    Initialize Flutter starter kit

---
+++ lib/consts.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+// A primary color definition
+const primaryColor = Color(0xFF00880D);
+
+// A secondary color definition
+const secondaryColor = Color(0xFF0281A0);
+
+const textPrimaryColor = Color(0xFF1D1D1D);
+const textColor = Color(0xFF4A4A4A);
+
+// animationDuration time definition
+const animationDuration = Duration(milliseconds: 200);
+
+// default constant value for padding size
+const defaultPadding = 20.0;
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:gojek/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(const Gojek());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
